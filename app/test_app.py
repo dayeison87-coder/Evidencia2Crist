f1a9dc77e32d04e82e86011b012e9916be9069f0
dayeison87 <dayeison87@gmail.com>
2026-09-14 23:03:43 -0500

    LISTO

---
+++ app/test_app.py
@@ -0,0 +1,11 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_health(client):
+    response = client.get('/health')
+    assert response.status_code == 200
